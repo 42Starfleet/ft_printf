6ac923d7534f79730a14f1d5351e9df9c70c027a
Hiroshi-U <33850376+Hiroshi-U@users.noreply.github.com>
2018-01-26 16:05:53 -0800

    Set precedence of "+" over "_"

--- set_flags.c
+++ set_flags.c
@@ -15,8 +15,10 @@ bool	set_flags(t_bag *bag, char c)
 		bag->plus = 1;
 		if (bag->zero == 1)
 			bag->zero = 0;
+		if (bag->space == 1)
+			bag->space = 0;
 	}
-	if (c == ' ')
+	if (c == ' ' && !bag->plus)
 		bag->space = 1;
 	if (c == '0' && bag->minus != 1 && bag->plus != 1)
 		bag->zero = 1;
