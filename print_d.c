652015d2b336fc5b6eaf33cc52405f212d95ee0e
Sergio Camargo <sergio.camargo813@gmail.com>
2018-01-26 16:42:07 -0800

    hotfix: printd compile issue

--- print_d.c
+++ print_d.c
@@ -4,9 +4,15 @@ void	print_d(t_bag *bag, va_list args, int *i)
 {
 	char	*str;
 	int		len;
-	// increase count by number of characters printed
+	
 	str = ft_itoa(va_arg(args, long));
 	len = ft_strlen(str);
+	// increase count by number of characters printed
+	// if precision not given, and zero flag on
+	if (!bag->precision && bag->zero)
+	{
+		// TODO: add zeroes based on width
+	}	
 	write(1, str, len);
 	*i += len;
 }
