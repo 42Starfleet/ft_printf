0d6bce06490e5c0ea368a5bcef504aca7226c7bf
Hiroshi-U <33850376+Hiroshi-U@users.noreply.github.com>
2018-01-27 12:38:23 -0800

    Update initialize_t_bag_variables.c

--- initialize_t_bag_variables.c
+++ initialize_t_bag_variables.c
@@ -7,8 +7,8 @@ void	initialize_t_bag_variables(t_bag *bag)
 	bag->minus = 0;
 	bag->plus = 0;
 	bag->space = 0;
-	bag->width = -1;
-	bag->precision = -1;
+	bag->width = 0;
+	bag->precision = 0;
 	bag->length_modifier = "";
 	bag->format_conversion = 0;
 }
