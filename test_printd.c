a32ead08549c6458a32d48b1d0a71c646ca41678
Sergio Camargo <sergio.camargo813@gmail.com>
2018-01-26 22:02:21 -0800

    handle zero flag -- basic

--- test_printd.c
+++ test_printd.c
@@ -15,8 +15,8 @@ void test_printfd()
 	printf("EXPECTED: '%0d'\n", 45);
 	ft_printf("RESULT: '%0d'\n", 45);
 	printf("0 flag with min width > num width\n");
-	printf("EXPECTED: '%03d'\n", 22);
-	ft_printf("RESULT: '%03d'\n", 22);
+	printf("EXPECTED: '%04d'\n", 22);
+	ft_printf("RESULT: '%04d'\n", 22);
 	n = ft_printf("Width: %3.3d\n", 2);
 	printf("Width: %5.4d\n", 235);
 	printf("Width: %04d\n", 245);
