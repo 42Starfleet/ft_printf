6a3de2024c03ff8bd1cc71429217c022f48955f4
Sergio Camargo <sergio.camargo813@gmail.com>
2018-01-29 22:36:37 -0800

    added d modifier functionality

--- print_d.c
+++ print_d.c
@@ -6,15 +6,45 @@ void	print_d(t_bag *bag, va_list args, int *i)
 	char	*str;
 	int		len;
 	int		n;
+	int		num;
 
-	str = ft_itoa(va_arg(args, long));
+	str = ft_itoa((num = va_arg(args, long)));
 	len = ft_strlen(str);
+	n = bag->width - len;
+	if (!bag->minus && (bag->precision || !bag->zero))
+	{
+		while (n-- > 0)
+		{
+			ft_putchar(' ');
+			(*i)++;
+		}
+	}
+	if (num < 0)
+	{
+		ft_putchar('-');
+		str++;
+		len--;
+		(*i)++;
+	}
+	else
+	{
+		if (bag->plus)
+		{
+			ft_putchar('+');
+			(*i)++;
+			n--;
+		}
+		else if (bag->space)
+		{
+			ft_putchar(' ');
+			(*i)++;
+		}
+	}
 	// increase count by number of characters printed
 	// if precision not given, and zero flag on
-	if (!bag->precision && bag->zero)
+	if (!bag-> minus && !bag->precision && bag->zero)
 	{
 		// TODO: add zeroes based on width
-		n = bag->width - len;
 		while (n-- > 0)
 		{
 			ft_putchar('0');
@@ -22,5 +52,10 @@ void	print_d(t_bag *bag, va_list args, int *i)
 		}
 	}
 	write(1, str, len);
+	while (n-- > 0)
+	{
+		ft_putchar(' ');
+		(*i)++;
+	}
 	*i += len;
 }
