f4df4929c81b5e1f424685450c28d54add36cd0b
Sergio Camargo <sergio.camargo813@gmail.com>
2018-02-01 16:24:37 -0800

    truncate %d len modifiers

--- print_d.c
+++ print_d.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_d.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/01 16:08:27 by scamargo          #+#    #+#             */
+/*   Updated: 2018/02/01 16:22:48 by scamargo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include <stdio.h>
 #include <inttypes.h>
 
-void	print_d(t_bag *bag, va_list args, int *i)
+void	print_d(t_bag *b, va_list args, int *i)
 {
 	char		*str;
 	char		*str_orig;
@@ -12,27 +24,21 @@ void	print_d(t_bag *bag, va_list args, int *i)
 	int			p;
 
 	num = va_arg(args, intmax_t);
-	if (!ft_strcmp(bag->length_modifier, "l") ||
-		bag->format_conversion == 'D')
+	if (!ft_strcmp(b->length_modifier, "l") || b->format_conversion == 'D' ||
+	!ft_strcmp(b->length_modifier, "ll") || !ft_strcmp(b->length_modifier, "z"))
 		num = (long)num;
-	else if (ft_strlen(bag->length_modifier) == 0)
-		num = (int)num;
-	else if (!ft_strcmp(bag->length_modifier, "h"))
-		num = (short)num;
-	else if (!ft_strcmp(bag->length_modifier, "hh"))
-		num = (signed char)num;
-	else if (!ft_strcmp(bag->length_modifier, "ll") || 
-			ft_strcmp(bag->length_modifier, "z"))
-		num = (long long)num;
+	num = (ft_strlen(b->length_modifier) == 0) ? (int)num : num;
+	num = (!ft_strcmp(b->length_modifier, "h")) ? (short)num : num;
+	num = (!ft_strcmp(b->length_modifier, "hh")) ? (signed char)num : num;
 	str = ft_intmax_toa(num);
 	str_orig = str;
 	len = ft_strlen(str);
 	len -= (num < 0) ? 1 : 0;
-	p = (bag->precision - len > 0) ? bag->precision - len : 0;
-	len = (num == 0 && bag->period && bag->precision == 0) ? 0 : len;
-	n = bag->width - len - p;
-	n -= (bag->plus || bag ->space || num < 0) ? 1 : 0;
-	if (!bag->minus && (bag->precision || !bag->zero))
+	p = (b->precision - len > 0) ? b->precision - len : 0;
+	len = (num == 0 && b->period && b->precision == 0) ? 0 : len;
+	n = b->width - len - p;
+	n -= (b->plus || b->space || num < 0) ? 1 : 0;
+	if (!b->minus && (b->precision || !b->zero))
 	{
 		while (n-- > 0)
 		{
@@ -46,20 +52,20 @@ void	print_d(t_bag *bag, va_list args, int *i)
 		str++;
 		(*i)++;
 	}
-	else	
+	else
 	{
-		if (bag->plus)
+		if (b->plus)
 		{
 			ft_putchar('+');
 			(*i)++;
 		}
-		else if (bag->space)
+		else if (b->space)
 		{
 			ft_putchar(' ');
 			(*i)++;
 		}
 	}
-	if (bag->precision)
+	if (b->precision)
 	{
 		while (p-- > 0)
 		{
@@ -67,7 +73,7 @@ void	print_d(t_bag *bag, va_list args, int *i)
 			(*i)++;
 		}
 	}
-	else if (!bag->minus && bag->zero)
+	else if (!b->minus && b->zero)
 	{
 		while (n-- > 0)
 		{
@@ -83,5 +89,4 @@ void	print_d(t_bag *bag, va_list args, int *i)
 	}
 	*i += len;
 	free(str_orig);
-	// TODO: see why you cant free str
 }
